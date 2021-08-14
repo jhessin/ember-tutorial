75a3096434f599c325648c7069e69bf6df4902dd
Jim Hessin <jhessin@gmail.com>
2021-08-13 21:48:58 -0600

    Reusable Components

--- app/components/rental/image.ts
+++ app/components/rental/image.ts
@@ -1,5 +1,13 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 interface RentalImageArgs {}
 
-export default class RentalImage extends Component<RentalImageArgs> {}
+export default class RentalImage extends Component<RentalImageArgs> {
+	@tracked isLarge: boolean = false;
+
+	@action toggleSize() {
+		this.isLarge = !this.isLarge;
+	}
+}
